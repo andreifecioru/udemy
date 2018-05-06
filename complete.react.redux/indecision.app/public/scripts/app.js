0ac0c4c109ed8faadae44e4a195eaf9a487e2980
Andrei Fecioru <afecioru@adobe.com>
2018-05-06 19:05:41 +0300

    [COMPLETE.REACT] - Basic template rendering with JSX

--- complete.react.redux/indecision.app/public/scripts/app.js
+++ complete.react.redux/indecision.app/public/scripts/app.js
@@ -1,10 +1,64 @@
 "use strict";
 
 var template = React.createElement(
-  "h1",
-  { id: "andrei" },
-  "Andrei is really, trully the best!"
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    { id: "andrei" },
+    "Andrei is really, trully the best!"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "This is some info"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "This is some other info"
+  ),
+  React.createElement(
+    "ol",
+    null,
+    React.createElement(
+      "li",
+      null,
+      "Item 1"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "Item 2"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "Item 3"
+    )
+  )
 );
+
+var templateTwo = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    "Andrei Fecioru"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Age: 36"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Location: Bucharest"
+  )
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- complete.react.redux/indecision.app/src/app.js
+++ complete.react.redux/indecision.app/src/app.js
@@ -1,4 +1,24 @@
-var template = <h1 id="andrei">Andrei is really, trully the best!</h1>
+var template = (
+  <div>
+    <h1 id="andrei">Andrei is really, trully the best!</h1>
+    <p>This is some info</p>
+    <p>This is some other info</p>
+    <ol>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+    </ol>
+  </div>
+);
+
+var templateTwo = (
+  <div>
+    <h1>Andrei Fecioru</h1>
+    <p>Age: 36</p>
+    <p>Location: Bucharest</p>
+  </div>
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
