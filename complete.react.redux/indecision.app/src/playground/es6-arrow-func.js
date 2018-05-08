2b136a48072df39cc6abf4db6a7d73193b016742
Andrei Fecioru <afecioru@adobe.com>
2018-05-08 22:21:48 +0300

    [COMPLETE.REACT] - Working with 'arrow' functions in ES6 (pt. 1)

--- complete.react.redux/indecision.app/src/playground/es6-arrow-func.js
+++ complete.react.redux/indecision.app/src/playground/es6-arrow-func.js
@@ -1,27 +1,21 @@
-'use strict';
-
-var square = function square(x) {
+const square = function (x) {
   return x * x;
 };
 
 // arrow functions are always annonymous
 // if you want to reference arrow functions 
 // you need to assign them to variables
-var squareArrow = function squareArrow(x) {
+const squareArrow = (x) => {
   return x * x;
-};
+}
 
 console.log('square(5) = ', square(5));
 console.log('squareArrow(5) = ', squareArrow(5));
 
 // arrow function expression
-var squareArrowExpr = function squareArrowExpr(x) {
-  return x * x;
-};
+const squareArrowExpr = (x) => x * x;
 console.log('squareArrowExpr(5) = ', squareArrowExpr(5));
 
 // CHALLENGE
-var getFirstName = function getFirstName(fullName) {
-  return fullName.split(' ')[0];
-};
+const getFirstName = (fullName) => fullName.split(' ')[0];
 console.log('First name: ', getFirstName('Andrei Fecioru'));
