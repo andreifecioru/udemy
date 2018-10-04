0d67f6f933d25851a2bdbfcb02df25b716a7fcf0
Andrei Fecioru <andrei.fecioru@gmail.com>
2018-10-04 22:08:58 +0300

    [GRAPHQL] - Using operation arguments

--- graphql/basics/src/index.js
+++ graphql/basics/src/index.js
@@ -5,46 +5,17 @@ import { GraphQLServer } from 'graphql-yoga'
 //   - custom types have an 'id' filed on them
 const typeDefs = `
     type Query {
-        me: User!
-        post: Post!
+        greeting(name: String!): String!
+        add(x: Float!, y: Float!): Float!
     }
 
-    type User {
-        id: ID!
-        name: String!
-        email: String!
-        age: Int
-    }
-
-    type Post {
-        id: ID!
-        title: String!
-        body: String!
-        published: Boolean!
-    }
 `
 
 // Resolvers
 const resolvers = {
     Query: {
-        me() {
-            return {
-                id: '0001',
-                name: 'Andrei',
-                email: 'andrei@example.com',
-                age: null
-            }
-        },
-
-        post() {
-            return {
-                id: '0002',
-                title: "Example post",
-                body: "Example post body",
-                published: false
-            }
-        }
-
+        greeting(parent, args, ctx, info) { return `Hello, ${args.name}!` },
+        add(parent, args, ctx, info) { return args.x + args.y }
     }
 
 }
