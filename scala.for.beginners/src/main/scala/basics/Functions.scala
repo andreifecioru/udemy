7d17fb9304a503e36412f57ac7b7ef2f0b8a05b4
Andrei Fecioru <andrei.fecioru@gmail.com>
2018-11-10 20:25:48 +0200

    [SCALA.FOR.BEGINNERS] - Functions

---
+++ scala.for.beginners/src/main/scala/basics/Functions.scala
@@ -0,0 +1,22 @@
+package basics
+
+object Functions extends App {
+  // exercise 1
+  println(s"[EX 1]: ${greeting("Andrei", 37)}")
+
+  // exercise 2
+  println(s"[EX 2]: 3! = ${factorial(3)}")
+  println(s"[EX 2]: 4! = ${factorial(4)}")
+
+  def greeting(name: String, age: Int): String =
+    s"Hi! My name is $name and I am $age years old."
+
+  def factorial(n: Int): Int = {
+    def reduce(acc: Int, x: Int): Int = x match {
+      case 1 => acc
+      case _ => reduce(acc * x, x - 1)
+    }
+
+    reduce(1, n)
+  }
+}
