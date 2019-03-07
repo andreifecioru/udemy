c31acc3775f30c9a4d9b28056ec6fbafbbd0a2e6
Andrei Fecioru <andrei.fecioru@gmail.com>
2019-03-07 20:36:49 +0200

    [MODERN.JS] - Lesson 03: Working with if statements

--- modern.js/lesson.003/if-statements.js
+++ modern.js/lesson.003/if-statements.js
@@ -1,3 +1,4 @@
+// Challenge: if statement
 let age = 7
 
 if (age <= 7) {
@@ -6,5 +7,16 @@ if (age <= 7) {
 
 if (age >= 65) {
     console.log('Applying the senior discount price.')
+}
+
+// Challenge: advanced if statement
+let temp = 35
+
+if (temp <= 10) {
+    console.log('It is cold outside.')
+} else if (temp >= 35) {
+    console.log('It is hot outside.')
+} else {
+    console.log('It is fine outside.')
 }
 
