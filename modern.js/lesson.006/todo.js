0e209f0b4d12bc75c963a2b48ee13a61ee298c3d
Andrei Fecioru <andrei.fecioru@gmail.com>
2019-03-09 12:45:37 +0200

    [MODERN.JS] - Lesson 06: Arrays basiscs

---
+++ modern.js/lesson.006/todo.js
@@ -0,0 +1,5 @@
+const todos = ['eat', 'sleep', 'play', 'work', 'watch tv']
+
+console.log(`You have ${todos.length} todos.`)
+console.log(`Todo: ${todos[0]}`)
+console.log(`Todo: ${todos[todos.length -2]}`)
