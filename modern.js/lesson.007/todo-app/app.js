b55c410698fa82db7a9f66102660eab2288f9b0a
Andrei Fecioru <afecioru@adobe.com>
2019-03-12 03:34:17 +0200

    [MODERN.JS] - Lesson 07: DOM manipulation challenge

--- modern.js/lesson.007/todo-app/app.js
+++ modern.js/lesson.007/todo-app/app.js
@@ -1,3 +1,11 @@
 console.log("TODO app");
 
-document.querySelectorAll("p").forEach(p => console.log(p.textContent));
+const paragraphs = document.querySelectorAll("p");
+
+// Remove all "p" tags that have "the" in their text
+// NOTE: "paragraphs" is a NoteList instance (not an array)
+Array.from(paragraphs)
+	.filter(p => p.textContent.includes("the"))
+	.forEach(p => p.remove());
+
+
