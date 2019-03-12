641f4de526b8bbcd741cb5be158f2ee93cc182ab
Andrei Fecioru <afecioru@adobe.com>
2019-03-12 03:20:41 +0200

    [MODERN.JS] - Lesson 07: Basic DOM manipulation

--- modern.js/lesson.007/notes-app/app.js
+++ modern.js/lesson.007/notes-app/app.js
@@ -1,1 +1,14 @@
-console.log("Andrei is the best");
+// Matches only the first paragraph occurrence
+const paragraph = document.querySelector("p");
+console.log(paragraph);
+
+// Match all paragraph occurrences
+const paragraphs = document.querySelectorAll("p");
+console.log(paragraphs);
+paragraphs.forEach(p => {
+    // p.remove();
+    // We can also change the paragraph text
+    // p.textContent = "******";
+
+    console.log(p.textContent);
+})
