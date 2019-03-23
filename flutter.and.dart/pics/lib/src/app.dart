7e9faaf521e3ed7def10aaef815e1840eaa44fa4
Andrei Fecioru <andrei.fecioru@gmail.com>
2019-03-23 11:37:16 +0200

    [FLUTTER.DART] - Lesson 09: Working with stateful widgets

--- flutter.and.dart/pics/lib/src/app.dart
+++ flutter.and.dart/pics/lib/src/app.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -9,11 +18,12 @@ class App extends StatelessWidget {
           title: Text("Let's See Images!"),
         ),
         body: Center(
-          child: Text('A pics app'),
+          child: Text('Counter value: $counter'),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            print('Hello!');
+            setState(() => counter ++);
+            print('Hello! $counter}');
           },
           tooltip: 'A button',
           child: Icon(Icons.add),
