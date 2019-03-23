254d82a700ee91a17b91933c1d031a9ae523c5db
Andrei Fecioru <andrei.fecioru@gmail.com>
2019-03-23 14:16:24 +0200

    [FLUTTER.DART] - Lesson 11: Working with JSON docs

---
+++ flutter.and.dart/lesson.011/json.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+void main() {
+  String rawJson = '{"name": "Andrei", "age": 25}';
+
+  var parsedJson = json.decode(rawJson);
+
+  Person person = Person.fromJson(parsedJson);
+
+  print(person);
+}
+
+class Person {
+  String name;
+  int age;
+
+  Person(this.name, this. age);
+
+  Person.fromJson(Map<String, dynamic> parsedJson) {
+    name = parsedJson['name'];
+    age = parsedJson['age'];
+  }
+
+  @override
+  String toString() {
+    return "Person($name, $age)";
+  }
+}
