6691b3f457910ce08351b9bba2ca90112d97cc34
Andrei Fecioru <andrei.fecioru@gmail.com>
2019-03-28 06:53:25 +0200

    [FLUTTER.DART] - Lesson 12: Created the 'login' app

---
+++ flutter.and.dart/login/lib/src/screens/login_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(32),
+      child: Form(
+        key: formKey,
+        child: Column(
+          children: <Widget>[
+            emailField(),
+            passwdField(),
+            SizedBox(height: 16),
+            submitBtn(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'e-mail',
+        hintText: 'your.name@example.com',
+      ),
+      keyboardType: TextInputType.emailAddress,
+    );
+  }
+
+  Widget passwdField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'password',
+        hintText: 'your password here'
+      ),
+      obscureText: true,
+    );
+
+  }
+
+  Widget submitBtn() {
+    return RaisedButton(
+      color: Colors.blue,
+      textColor: Colors.white,
+      child: Text('Submit'),
+      onPressed: onSubmitPressed,
+    );
+  }
+
+  void onSubmitPressed() {
+    formKey.currentState.reset();
+  }
+}
