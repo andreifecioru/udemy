c28e6e609dc1614d8efddab2c3768c9c567e4f27
Andrei Fecioru <afecioru@adobe.com>
2019-04-08 17:24:31 +0300

    [SCALA.FOR.BEGINNERS] - Implementing filter, map, flatMap for the custom list

--- scala.for.beginners/src/main/scala/oop/exercises/CustomList.scala
+++ scala.for.beginners/src/main/scala/oop/exercises/CustomList.scala
@@ -4,19 +4,63 @@ import scala.annotation.tailrec
 
 object CustomList extends App {
 
-  val list = MyList(1, 2, 3)
+  val list = MyList(1, 2, 3, 4, 5, 6)
   println(list)
 
   val anotherList = MyList()
   println(anotherList)
 
+  val even = new MyPredicate[Int] {
+    override def test(elem: Int): Boolean = (elem % 2) == 0
+  }
+
+  val square = new MyTransformation[Int, Int] {
+    override def transform(elem: Int): Int = elem * elem
+  }
+
+  val duplicate = new MyTransformation[Int, Node[Int]] {
+    override def transform(elem: Int): Node[Int] = ValueNode(elem, ValueNode(elem, NilNode))
+  }
+
+  val filteredList = list.filter(even)
+  println(s"Filtered list:  $filteredList")
+
+  val squaredList = list.map(square)
+  println(s"Squared list: $squaredList")
+
+  val duplicatedList = list.flatMap(duplicate)
+  println(s"Duplicated list: $duplicatedList")
+
+
   // --------------------------
+
   sealed trait Node[+T] {
     def isEmpty: Boolean
     def head: T
     def tail: Node[T]
 
-    def add[U >: T](value: U): ValueNode[U] = new ValueNode(value, this)
+    def add[U >: T](value: U): ValueNode[U] = ValueNode(value, this)
+
+    def addAll[U >: T](other: Node[U]): Node[U] = other match {
+      case NilNode => this
+      case ValueNode(h, t) => ValueNode(h, this).addAll(t)
+    }
+
+    def filter(p: MyPredicate[T]): Node[T] = this match {
+      case NilNode => NilNode
+      case ValueNode(h, t) if p.test(h) => ValueNode(h, t.filter(p))
+      case ValueNode(_ , t) => t.filter(p)
+    }
+
+    def map[U](f: MyTransformation[T, U]): Node[U] = this match {
+      case NilNode => NilNode
+      case ValueNode(h, t) => ValueNode(f.transform(h), t.map(f))
+    }
+
+    def flatMap[U](f: MyTransformation[T, Node[U]]): Node[U] = this match {
+      case NilNode => NilNode
+      case ValueNode(h, t) => f.transform(h).addAll(t.flatMap(f))
+    }
 
     override def toString: String = {
       @tailrec
@@ -30,7 +74,7 @@ object CustomList extends App {
     }
   }
 
-  class ValueNode[T](override val head: T, override val tail: Node[T]) extends Node[T] {
+  case class ValueNode[T](override val head: T, override val tail: Node[T]) extends Node[T] {
     override def isEmpty: Boolean = false
   }
 
@@ -43,5 +87,13 @@ object CustomList extends App {
   object MyList {
     def apply[T]: Node[T] = NilNode
     def apply[T](values: T*): Node[T] = values.foldLeft(NilNode.asInstanceOf[Node[T]])(_.add(_))
+  }
+
+  trait MyPredicate[-T] {
+    def test(elem: T): Boolean
+  }
+
+  trait MyTransformation[-T, U] {
+    def transform(elem: T): U
   }
 }
