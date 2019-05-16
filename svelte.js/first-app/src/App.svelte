36edc59d6265f3f9818383b4684a76e591b8b5a1
Andrei Fecioru <afecioru@adobe.com>
2019-05-16 17:10:25 +0300

    [SVELTE.JS] - Created a very simple interactive app (with one button)

---
+++ svelte.js/first-app/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+	export let name;
+	export let age;
+
+	function incrementAge() {
+		age += 1;
+	}
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name}!</h1>
+<p>I am {age} years old.</p>
+
+<button on:click="{incrementAge}">Change age</button>
