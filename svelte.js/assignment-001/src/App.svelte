365af36d51975b3852132a62414a8f146171ed8c
Andrei Fecioru <andrei.fecioru@gmail.com>
2019-05-16 22:12:32 +0300

    [SVELTE.JS][ASSIGN.001] | Implementing a basic Svelte component"

---
+++ svelte.js/assignment-001/src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import GoalComponent from './GoalComponent.svelte'
+
+  let goalName='Goal is not set yet.'
+
+  const updateGoal = (event) => goalName = event.target.value;
+</script>
+
+<GoalComponent {goalName} />
+
+<input type="text" on:input={updateGoal}>
+
+<hr>
+
+<p>Solve these tasks.</p>
+
+<ol>
+  <li>Add an input field that allows users to enter a course goal.</li>
+  <li>Output the user input in a h1 tag.</li>
+  <li>
+    Color the output red (e.g. by adding a class) if it contains at least one exclamation mark.
+  </li>
+  <li>
+    Put the h1 tag + output into a separate component to which you pass the user input
+  </li>
+</ol>
---
+++ svelte.js/assignment-001/src/GoalComponent.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let goalName='';
+
+  $: hasExclamation = goalName.includes('!');
+</script>
+
+<style>
+  .with-exclamation {
+    color: red;
+  }
+</style>
+
+<h1 class:with-exclamation={hasExclamation}>{goalName}</h1>
