f23030760c80b53f041ef387d367b93696e452e2
Andrei Fecioru <afecioru@adobe.com>
2019-05-16 18:17:44 +0300

    [SVELTE.JS] - Reactive statements, vars and HTML element bindings.

--- svelte.js/first-app/src/App.svelte
+++ svelte.js/first-app/src/App.svelte
@@ -2,8 +2,21 @@
 	export let name;
 	export let age;
 
-	function incrementAge() {
-		age += 1;
+	const incrementAge = () => age += 1;
+
+	// reactive vars
+	// NOTE: var definition is not requried
+	$: uppercaseName = name.toUpperCase();
+
+	// reactive statements
+	$: console.log(`${name} is ${age} yeas old.`);
+
+	$: if (age % 10 == 0) {
+		console.log('You are older!');
+	}
+
+	const nameInput = (event) => {
+		name = event.target.value;
 	}
 </script>
 
@@ -13,7 +26,17 @@
 	}
 </style>
 
-<h1>Hello {name}!</h1>
+<h1>Hello {uppercaseName}!</h1>
 <p>I am {age} years old.</p>
 
 <button on:click="{incrementAge}">Change age</button>
+<br>
+<br>
+<br>
+
+<!-- 2-way binding to an element attribute 
+	<input type="text" value="{name}" on:input="{nameInput}"> 
+	For a shortcut see below:
+-->
+<input type="text" bind:value="{name}">
+
