dceb9bcb6377b5932701a2c6b66741f32d25f9b9
Andrei Fecioru <andrei.fecioru@gmail.com>
2019-06-08 09:02:01 +0300

    [SVELTE.JS] - Passing component props via object spread operator

--- svelte.js/events/src/App.svelte
+++ svelte.js/events/src/App.svelte
@@ -3,20 +3,38 @@
 
   const onProductAdd = event => {
     // event.detail contains the data attached to the event.
-    console.log("Product was added! Attached data: " + event.detail);
+    console.log("Product was added! Attached data: " + event.detail.id);
   };
 
   const onProductRemove = event => {
-    console.log("Product was deleted! Attached data: " + event.detail);
+    console.log("Product was deleted! Attached data: " + event.detail.id);
   };
+
+  let products = [
+    {
+      id: Math.random(),
+      title: "A book",
+      price: 9.99
+    },
+    {
+      id: Math.random(),
+      title: "A car",
+      price: 9999.99
+    }
+  ];
 </script>
 
 <style>
 
 </style>
 
-<!-- Handle custome events dispatched by the Product component -->
-<Product
-  productTitle="A book"
-  on:add-to-cart={onProductAdd}
-  on:remove-from-cart={onProductRemove} />
+<!-- 
+	Handle custom events dispatched by the Product component.
+	NOTE: you can pass in prop data using the spread operator on objects.
+-->
+{#each products as product}
+  <Product
+    {...product}
+    on:add-to-cart={onProductAdd}
+    on:remove-from-cart={onProductRemove} />
+{/each}
--- svelte.js/events/src/Product.svelte
+++ svelte.js/events/src/Product.svelte
@@ -1,18 +1,20 @@
 <script>
   import { createEventDispatcher } from "svelte";
 
-  export let productTitle;
+  export let id;
+  export let title;
+  export let price;
 
   const onClick = (event) => {
     console.log("Something was clicked!")
   };
 
   const addToCart = (event) => {
-    dispatch('add-to-cart', "some data");
+    dispatch('add-to-cart', {id: id});
   };
 
   const removeFromCart = (event) => {
-    dispatch('remove-from-cart', "some other data")
+    dispatch('remove-from-cart', {id: id});
   };
 
   const dispatch = createEventDispatcher();
@@ -20,7 +22,8 @@
 
 <!-- You can also listen for events here (via DOM event propagation) -->
 <article on:click="{onClick}">
-  <h1>{productTitle}</h1>
+  <h1>{title}</h1>
+  <h2>${price}</h2>
   <!-- Fire some custom events when buttons are clicked -->
   <button on:click="{addToCart}">Add to cart</button>
   <button on:click="{removeFromCart}">Delete</button>
