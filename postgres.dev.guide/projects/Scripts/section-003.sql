237b80b13d26235806f2b77dede88930f8b04ff4
Andrei Fecioru <andrei.fecioru@gmail.com>
2020-10-30 11:13:40 +0200

    [PG-SQL.DEV.GUIDE] - Foreign keys and constraints.

---
+++ postgres.dev.guide/projects/Scripts/section-003.sql
@@ -0,0 +1,26 @@
+-- Run photo-app.sql first to seed the DB with data
+
+SELECT * FROM users;
+SELECT * FROM photos;
+
+SELECT * FROM photos WHERE user_id = 1;
+
+-- basic joining of 2 tables
+SELECT username, url
+FROM photos JOIN users
+ON photos.user_id = users.id;
+
+-- cannot insert a photo referencing a non-existent user
+INSERT INTO photos (url, user_id)
+VALUES
+    ('http://gigi/img_001.jpg', 100);
+
+-- we can insert a photo w/o specifying a user (if the user_id col is marked as nullable)
+INSERT INTO photos (url)
+VALUES ('http://anon/img_001.jpg');
+
+-- when deleting a user, the photos associated with it are also deleted
+-- because this is how we configured the FK to behave
+DELETE FROM users WHERE username = 'andrei';
+
+    
